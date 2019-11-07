e64b713eee38040a907cf67feb60f055eeb6ab43
Uma Sudhan P <umasudhan.p@gmail.com>
2019-11-07 15:43:36 +1100

    Update test-validation.js

--- test-validation.js
+++ test-validation.js
@@ -7,12 +7,12 @@ module.exports = function(RED) {
 
     RED.httpAdmin.get("/birds", (req, res) => {
         const birds = ['bird-1', 'bird-2', 'bird-3'];
-        console.log('selected bird name:', birdName);
-        const filtered = birds.filter(value=>{ //remove selected bird to simulate dynamic backend
-            return value!==birdName;
-        });
-        console.log('going to return birds as:',filtered);
-        res.json(filtered);
+        const min = 0;
+        const max = 2;
+        const random = Math.floor(Math.random() * (+max - +min)) + +min;
+        birds.splice(random, 1); //randomly remove an element to simulate a dynamic backend
+        console.log('going to return birds as:',birds);
+        res.json(birds);
     });
 
     RED.nodes.registerType("test-validation",TestValidation);
